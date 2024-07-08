1b790d7221e37972f3b9d633108becd7d92d0cd9
stgro <st.groshev96@gmail.com>
2024-07-08 20:35:16 +0300

    Исправил замечания)

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -3,24 +3,21 @@ package com.practicum.playlistmaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        var intent:Intent
+        var intent: Intent
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val buttonSearch = findViewById<MaterialButton>(R.id.button_search)
         val buttonMedia = findViewById<MaterialButton>(R.id.button_media)
         val buttonSettings = findViewById<MaterialButton>(R.id.button_settings)
-        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-            intent = Intent(this@MainActivity, SearchActivity::class.java)
-                startActivity(intent)
-            }
+
+        buttonSearch.setOnClickListener {
+            intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
         }
-        buttonSearch.setOnClickListener(buttonSearchClickListener)
 
         buttonMedia.setOnClickListener {
             intent = Intent(this, MediaActivity::class.java)
