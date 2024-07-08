9d7b6c6e4df61b827e6eac4157bf902e7427176c
stgro <st.groshev96@gmail.com>
2024-07-08 15:54:24 +0300

    Реализовал нажатие на кнопки с поялением тоста

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -2,10 +2,30 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        setContentView(R.layout.activity_main)
+        val buttonSearch = findViewById<MaterialButton>(R.id.button_search)
+        val buttonMedia = findViewById<MaterialButton>(R.id.button_media)
+        val buttonSettings = findViewById<MaterialButton>(R.id.button_settings)
+        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на поиск...", Toast.LENGTH_SHORT).show()
+            }
+        }
+        buttonSearch.setOnClickListener(buttonSearchClickListener)
+
+        buttonMedia.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на медиатеку...", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonSettings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на настройки...", Toast.LENGTH_SHORT).show()
+        }
     }
 }
