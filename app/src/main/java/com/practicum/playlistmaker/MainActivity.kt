7f577d204b0b99e2b815eb3a91aa692b7520efa8
stgro <st.groshev96@gmail.com>
2024-07-10 11:43:21 +0300

    Поигрался со стилями, темой, шрифтами

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -3,16 +3,17 @@ package com.practicum.playlistmaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.button.MaterialButton
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         var intent: Intent
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val buttonSearch = findViewById<MaterialButton>(R.id.button_search)
-        val buttonMedia = findViewById<MaterialButton>(R.id.button_media)
-        val buttonSettings = findViewById<MaterialButton>(R.id.button_settings)
+
+        val buttonSearch = findViewById<Button>(R.id.button_search)
+        val buttonMedia = findViewById<Button>(R.id.button_media)
+        val buttonSettings = findViewById<Button>(R.id.button_settings)
 
         buttonSearch.setOnClickListener {
             intent = Intent(this, SearchActivity::class.java)
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -3,6 +3,7 @@ package com.practicum.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 class SettingsActivity: AppCompatActivity() {
 
@@ -12,9 +13,11 @@ class SettingsActivity: AppCompatActivity() {
 
         val back = findViewById<ImageView>(R.id.back)
 
+
         back.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
     }
 }
